11330505266f289aa6c1fbc07dfcb1ce1e570b7c
OmniaAbdelrahman <52755875+OmniaAbdelrahman@users.noreply.github.com>
2021-12-14 16:41:44 +0200

    Add files via upload

---
+++ Stock Price APP.py
@@ -0,0 +1,37 @@
+import yfinance as yf
+import streamlit as st
+#Markdown Language
+
+st.write("""
+# **Simple Stock Price App**
+
+Shown are the stock closing price and volume of Apple!
+
+[Apple stock is on fire but will it last? - Yahoo Finance](https://finance.yahoo.com/news/apple-stock-is-on-fire-but-will-it-last-200735528.html)
+
+
+That is so Great! :smiley:
+
+""")
+
+# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
+#define the ticker symbol
+tickerSymbol = 'AAPL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+
+
+st.write("""
+## Closing Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
+
+#https://www.youtube.com/watch?v=JwSS70SZdyM
